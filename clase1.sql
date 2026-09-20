fa3a46546ad4850388a398d3b6b88c0e746cadda
servel777 <servel777@gmail.com>
2026-09-20 09:41:11 -0400

    trabajando con los queries delete update

--- clase1.sql
+++ clase1.sql
@@ -33,8 +33,71 @@ insert into producto(Nombre,Precio,stock,Idcategoria) values('camiseta',50.40,50
 insert into producto(Nombre,Precio,stock,Idcategoria) values('pantalon',50.40,50,1)
 insert into producto(Nombre,Precio,stock,Idcategoria) values('zapatilla',50.40,50,3)
 insert into producto(Nombre,Precio,stock,Idcategoria) values('lentes de sol',50.40,50,2)
+insert into producto(Nombre,Precio,stock,Idcategoria) values('tennis',50.40,50,3)
+insert into producto(Nombre,Precio,stock,Idcategoria) values('cartera',50.40,10,1)
+insert into producto(Nombre,Precio,stock,Idcategoria) values('chaqueta',50.30,50,3)
+insert into producto(Nombre,Precio,stock,Idcategoria) values('botas',50.30,5,2)
+insert into producto(Nombre,Precio,stock,Idcategoria) values('gorra',50.30,20,2)
 
 select * from Producto;
 select p.Nombre,p.Precio,c.Nombre from Producto p
 inner join Categoria c on p.Idcategoria=c.IdCategoria
 
+  select * from producto where stock<=5 or Nombre='camiseta'
+ select * from producto where stock>=5 or Nombre='camiseta'
+
+
+ select * from producto
+ order by nombre desc
+
+
+ select * 
+ from producto
+ where precio>51
+ order by precio desc
+
+ --like
+ select *
+ from producto
+ where nombre like '%eta'
+
+ select * from producto where precio>51
+
+ --between
+ select *
+ from producto
+ where precio between 50 and 70
+
+
+ select * from producto
+ where stock between 10 and 50 
+
+ --modificar
+ 
+ select * from producto
+ where nombre ='camiseta'
+
+ update producto
+ set precio=60.50,
+     stock=40
+ where nombre='camiseta'
+
+ update producto
+ set Idcategoria=2
+ where nombre='zapatilla'
+
+  update producto
+ set Idcategoria=3
+ where nombre='lentes de sol'
+ 
+ select p.idProducto,p.Nombre,p.Precio,p.stock,c.Nombre from Producto p
+inner join Categoria c on p.Idcategoria=c.IdCategoria
+
+update producto
+set nombre='gafas de sol'
+where nombre='camiseta'
+
+--delete
+select * from producto
+delete producto
+where nombre='pantalon'  
--- ejemplo1.cs
+++
@@ -1,7 +0,0 @@
-public class Ejemplo1{
-    public static void Main(string[]args){
-        int a=3,b=4,c=0;
-        c=a+b;
-        Console.WriteLine($"el resultado es {c}");
-    }
-}
